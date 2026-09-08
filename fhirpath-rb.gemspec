413b85ef9bb36630dc8c249821cdda32545aa9f5
Ilya Beda <ir4y.ix@gmail.com>
2026-09-08 11:57:42 +1000

    Expose public API

--- fhirpath-rb.gemspec
+++ fhirpath-rb.gemspec
@@ -32,6 +32,8 @@ Gem::Specification.new do |spec|
   spec.executables = spec.files.grep(%r{\Aexe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
+  spec.add_dependency "fhir_models", "~> 5.1"
+
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
 end
--- lib/fhirpath.rb
+++ lib/fhirpath.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
+require "fhir_models"
+
 require_relative "fhirpath/version"
 
 module Fhirpath
   class Error < StandardError; end
+
+  def self.evaluate(_resource, _path, _context = {}, _model = nil, _options = {})
+    []
+  end
+
+  def self.compile(_path, _model = nil, _options = {})
+    ->(_resource, _context = nil) { [] }
+  end
+
+  def self.compile_as_array(expression, model = nil)
+    compile(expression, model)
+  end
+
+  def self.compile_as_first(expression, model = nil)
+    path = compile_as_array(expression, model)
+    ->(resource, context = nil) { path.call(resource, context).first }
+  end
 end
